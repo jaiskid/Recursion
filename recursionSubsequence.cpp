c5429aaeddafac5f08c467bf485e5384ab2bde84
Jaiskid <neeraj001jaiswal@gmail.com>
2019-09-19 00:35:36 +0530

    codeupdate

---
+++ recursionSubsequence.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+void subsequence(string q,string ans){
+	if(q.length()==0){
+		cout<<ans<<" ";
+	return ;
+	}
+	char ch=q[0];
+	string ros=q.substring(1);
+	subsequence(ros,ans+ch);
+	subsequence(ros,ans);
+}
+int  main(){
+	string s;
+	cin>>s;
+	subsequence(s," ");
+}
